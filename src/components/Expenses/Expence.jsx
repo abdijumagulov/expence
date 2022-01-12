ab22e7427f5f2857c84f37a3389f3564afcbc946
Abdirahman <abdirahman.jumagulov.kk@mail.com>
2022-01-12 16:04:01 +0600

    hello

--- src/components/Expenses/Expence.jsx
+++ src/components/Expenses/Expence.jsx
@@ -10,6 +10,8 @@ function Expence(props) {
           return (
             <Card className="expenses">
               <ExpenceItem title={el.title} amount={el.amount} date={el.date} img={el.img}/>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident quos, inventore sed ratione voluptatum aliquid at explicabo quam error iste perferendis, nulla quaerat assumenda dolore quasi consequuntur, rerum minus sequi adipisci cumque non numquam voluptatem nemo quidem! Unde, tenetur nemo!
+              </p>
             </Card>
           )
         })}
